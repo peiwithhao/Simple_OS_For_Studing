95fca4f3b4baef1f2cc676b716104a055b2e9875
PeiandHao <2689271477@qq.com>
2023-01-05 20:49:43 -0500

    will finish

---
+++ boot/assemble.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+nasm -I include/ -o mbr.bin mbr.S
+dd if=./mbr.bin of=../bochs/hd60M.img bs=512 count=1 conv=notrunc
+nams -I include -o loader.bin loader.S
+dd if=./loader.bin of=../bochs/hd60M.img bs=512 count=4 seek=2 conv=notrunc
