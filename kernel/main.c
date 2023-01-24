2f1a0214d6ea6419c111561327e5a3c5333f340a
PeiandHao <2689271477@qq.com>
2023-01-24 08:15:12 -0500

    testing

--- kernel/main.c
+++ kernel/main.c
@@ -13,6 +13,7 @@ int main(void){
   thread_start("k_thread_a", 31, k_thread_a, "argA");
   thread_start("k_thread_b", 8, k_thread_b, "argB");
   intr_enable();
+  put_str("intr_enable done\n");
   while(1);
 
   return 0;
