9610d522b3c1d1a182c94f57a76b8783e36be490
PeiandHao <2689271477@qq.com>
2023-01-26 10:42:19 -0500

    explain GP

--- kernel/main.c
+++ kernel/main.c
@@ -9,11 +9,13 @@ void k_thread_b(void*);
 int main(void){
   put_str("I am Kernel\n");
   init_all();
-  thread_start("k_thread_a", 31, k_thread_a, "argA");
-  thread_start("k_thread_b", 8, k_thread_b, "argB");
+  thread_start("k_thread_a", 31, k_thread_a, "argA ");
+  thread_start("k_thread_b", 8, k_thread_b, "argB ");
   intr_enable();
   while(1){
+    intr_disable();
     put_str("Main ");
+    intr_enable();
   };
   return 0;
 }
@@ -23,15 +25,17 @@ void k_thread_a(void* arg){
   /* 这里传递通用参数，这里被调用函数自己需要什么类型的就自己转换 */
   char* para = arg;
   while(1){
+    intr_disable();
     put_str(para);
-    put_str("a ");
+    intr_enable();
   }
 }
 void k_thread_b(void* arg){
   /* 这里传递通用参数，这里被调用函数自己需要什么类型的就自己转换 */
   char* para = arg;
   while(1){
+    intr_disable();
     put_str(para);
-    put_str("b ");
+    intr_enable();
   }
 }
