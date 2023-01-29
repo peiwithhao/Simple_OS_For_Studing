2dbdee6c0b686d1d6f5a7f4218cc1356c8933b54
PeiandHao <2689271477@qq.com>
2023-01-28 21:42:17 -0500

    finish keyboard

--- device/keyboard.c
+++ device/keyboard.c
@@ -100,15 +100,101 @@ static char keymap[][2] = {
   /* 0x37 */    {'*', '*'},
   /* 0x38 */    {alt_l_char, alt_l_char},
   /* 0x39 */    {' ', ' '},
-  /* 0x3A */    {caps_lock_status, caps_lock_status}
+  /* 0x3A */    {caps_lock_char, caps_lock_char}
   /* 其他按键暂不处理 */
 };
 
 /* 键盘中断处理程序 */
 static void intr_keyboard_handler(void){
-  uint8_t scancode = inb(KBD_BUF_PORT);
-  put_int(scancode);
-  return;
+  /* 这次中断发生前的上一次中断，以下任意三个键是否有人按下 */
+  bool ctrl_down_last = ctrl_status;
+  bool shift_down_last = shift_status;
+  bool caps_lock_last = caps_lock_status;
+
+  bool break_code;
+  uint16_t scancode = inb(KBD_BUF_PORT);
+
+  /* 若扫描码scancode是e0开头的，表示此键的按下将产生多个扫描码，
+   * 所以马上结束此中断处理函数，等待下一个扫描码进入*/
+  if(scancode == 0xe0){
+    ext_scancode = true;    //打开e0标记
+    return;
+  }
+  
+  /* 如果上次是以0xe0开头的，将扫描码合并 */
+  if(ext_scancode){
+    scancode = ((0xe000)|(scancode));
+    ext_scancode = false;   //关闭e0标记
+  }
+
+  break_code = ((scancode & 0x0080) != 0);  //获取breakcode，为false则表示是通码，为true则表示是断码
+  if(break_code){       //如果是断码breakcode(也就是弹起时产生)
+    /* 由于ctrl_r和alt_r的make_code和break_code都是两字节，所以可以用下面的方法取make_code，多字节的扫描码暂不处理 */
+    uint16_t make_code = (scancode &= 0xff7f);  //这里获取其通码
+
+    /* 如果以下任意三个键弹起了，将状态置为false */
+    if(make_code == ctrl_r_make || make_code == ctrl_l_make){
+      ctrl_status = false;
+    }else if(make_code == shift_r_make || make_code == shift_l_make){
+      shift_status = false;
+    }else if(make_code == alt_l_make || make_code == alt_r_make){
+      alt_status = false;
+    } /* 由于caps_lock不是弹起后关闭，所以需要单独处理 */
+    return;     //直接返回结束此次中断程序
+  }
+  /* 若为通码，只处理数组中定义的建以及alt_right和ctrl键，全是make_code */
+  else if((scancode > 0x00 && scancode < 0x3b)||(scancode == alt_r_make)||(scancode == ctrl_r_make)){
+    bool shift = false;     //判断是否与shift组合，用来在二维数组中索引对应的字符
+    if((scancode < 0x0e)||(scancode == 0x29)||(scancode == 0x1a)|| \
+        (scancode == 0x1B)||(scancode == 0x2B)||(scancode == 0x27)|| \
+        (scancode == 0x28)||(scancode == 0x33)||(scancode == 0x34)|| \
+        (scancode == 0x35)){
+      /********* 代表两个字母的键 *********
+       * 0x0e 数字0～9,字符-，字符=
+       * 0x29 字符`
+       * 0x1a 字符[
+       * 0x1b 字符]
+       * 0x2b 字符\\
+       * 0x27 字符;
+       * 0x28 字符\'
+       * 0x33 字符,
+       * 0x34 字符.
+       * 0x35 字符/
+       * **********************************/
+      if(shift_down_last){
+        shift = true;
+      }
+    }else{  //处理默认字母键
+      if(shift_down_last && caps_lock_last){    //如果shift和cpaslock同时按下
+        shift = false;
+      }else if(shift_down_last || caps_lock_last){
+        //如果shift和capslock任意被按下
+        shift = true;
+      }else{
+        shift = false;
+      }
+    }
+    uint8_t index = (scancode &= 0x00ff);
+    char cur_char = keymap[index][shift];   //在数组中寻找对应的字符
+    /* 只处理ASCII码不为0的键 */
+    if(cur_char){
+      put_char(cur_char);
+      return;
+    }
+    /* 记录本次是否按下了下面几类控制键之一，供下次键入的时候判断组合键 */
+    if(scancode == ctrl_l_make || scancode == ctrl_r_make){
+      ctrl_status = true;
+    }else if(scancode == shift_l_make || scancode == shift_r_make){
+      shift_status = true;
+    }else if(scancode == alt_l_make || scancode == alt_r_make){
+      alt_status = true;
+    }else if(scancode == caps_lock_make){
+      /* 不管之前是否有按下caps_lock键，当再次按下时状态取反 */
+      caps_lock_status = ! caps_lock_status;
+    }
+  }else{
+    put_str("unknown key\n");
+  }
 }
 
 /* 键盘初始化 */
